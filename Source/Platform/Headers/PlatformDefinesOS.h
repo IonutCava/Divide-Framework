59afd45b13c7f68cceefe9933dae712b01bbfddc
Ionut Cava <ionut.cava@divide-studio.com>
2025-06-07 11:10:44 +0100

    Don't know why we get missing intrinsincs now on Linux GitHub actions, but this (might?) fix it?

--- Source/Platform/Headers/PlatformDefinesOS.h
+++ Source/Platform/Headers/PlatformDefinesOS.h
@@ -33,6 +33,12 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #ifndef DVD_PLATFORM_DEFINES_OS_H_
 #define DVD_PLATFORM_DEFINES_OS_H_
 
+#ifdef USING_MSVC
+#include <intrin.h>
+#else // USING_MSVC
+#include <x86intrin.h>
+#endif // USING_MSVC
+
 #if defined(IS_WINDOWS_BUILD)
 #include "PlatformDefinesWindows.h"
 #elif defined(IS_MACOS_BUILD) || defined(IS_LINUX_BUILD)
